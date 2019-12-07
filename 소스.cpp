daa2b743e4be3cf0c92cdb97ba4a0fe573f5823d
nasemY <58096186+nasemY@users.noreply.github.com>
2019-12-07 11:31:38 +0900

    20191207(1)
    피곤아치

--- 소스.cpp
+++ 소스.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int fac(int n) {
+	if (n == 0) return 0;
 	if (n == 1) return 1;
-	return n * fac(n - 1);
+	return fac(n - 1) + fac(n - 2);
+	
+	
 }
 
 int main() {
-	int a;
-
-	scanf_s("%d", &a);
-	printf("%d", fac(a));
+	int n;
+	scanf_s("%d", &n);
+	printf("%d\n", fac (n));
+	return 0;
 }
