d107e679f24de2ede5d427875884ad9b25cca70a
nasemY <58096186+nasemY@users.noreply.github.com>
2019-12-07 11:44:49 +0900

    20191207

---
+++ 소스1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fac(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return fac(n - 1) + fac(n - 2);
+
+
+}
+
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	printf("%d\n", fac(n));
+	return 0;
+}
