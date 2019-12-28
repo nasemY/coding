5f1bb5e9aee78a580588bc26b08ee6eac68f2b3f
nasemY <58096186+nasemY@users.noreply.github.com>
2019-12-28 10:32:09 +0900

    Update 문자열 함수-미완성.cpp

--- 문자열 함수-미완성.cpp
+++ 문자열 함수-미완성.cpp
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int strlength(char* str) {
-	int len=0;
-	for (int i = 0;str[i]!=0 ;i++) {
+	int len = 0;
+	for (int i = 0;str[i] != 0;i++) {
 		len++;
 	}
-return len;
+	return len;
 }
 
 int strcmp1(char* str1, char*str2) {
-	int a = 1;
-	for (int j = 0; str1[j] != str2[j]; j++) {
-		if (str1[j] == str2[j]) {
-			a = 0;
-			printf(0);
-		}
-	}printf("%d", a);
+	int len = strlength(str1);
+	int len2 = strlength(str2);
+	if (len - len2)return 1;
+	for (int i = 0; i < len; i++) {
+		if (str1[i] != str2[i]) return 1;
+	}
 	return 0;
 }
 
@@ -23,6 +22,6 @@ int main() {
 	char str1[100];
 	char str2[100];
 	scanf("%s %s", str1, str2);
-	printf("%d", strcmp1(str1,str2));
+	printf("%d", strcmp1(str1, str2));
 	return 0;
 }
